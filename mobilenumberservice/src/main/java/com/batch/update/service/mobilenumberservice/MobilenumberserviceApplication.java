db6cbe1406ab526cc48ac719dd80105eccc60dba
P7111829 <P7111829@BLR-P7111829.NESSINBLR.COM>
2020-06-01 19:16:42 +0530

    Initial Commit

---
+++ mobilenumberservice/src/main/java/com/batch/update/service/mobilenumberservice/MobilenumberserviceApplication.java
@@ -0,0 +1,13 @@
+package com.batch.update.service.mobilenumberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilenumberserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilenumberserviceApplication.class, args);
+	}
+
+}
---
+++ mobilenumberservice/src/test/java/com/batch/update/service/mobilenumberservice/MobilenumberserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.update.service.mobilenumberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilenumberserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
